a296c133b342401a4944d4bb6a73413d64963d0d
Cristoph <cristopher.alejandro@gmail.com>
2016-04-20 17:55:40 -0300

    gui

---
+++ T-SAKPLATA/src/APP_Main/TSAKPLATA.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package APP_Main;
+
+import View.MainJFrame;
+import java.awt.Color;
+
+public class TSAKPLATA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // add test data
+
+        // Load Main View ... Start
+        MainJFrame mainView = new MainJFrame();
+        mainView.getContentPane().setBackground(Color.WHITE);
+        mainView.setVisible(true);
+
+    }
+
+}
